d5709c8d8f968eceb553ef37886ea8eab1673e6a
Kihura <76404328+Kihura@users.noreply.github.com>
2020-12-27 12:56:06 +0100

    Update Cavalier.java

--- src/cso/Cavalier.java
+++ src/cso/Cavalier.java
@@ -1,61 +1,105 @@
 package cso ;
 
-public class Cavalier {
+public class Cavaliers {
 	String nom;
-	int x;
-	int y;
-	float tailleCheval;
-	 int point;
-	public  Cavalier(String nom,int x,int y,float taille,int point) {
+	double tailleCheval;
+	int score;
+	public  Cavalier(String nom,float taille,int score) {
 		this.nom=nom;
-		this.x=x;
-		this.y=x;
 		this.tailleCheval=taille;
-		this.point=point;
+		this.score=score;
 	}
 	
-	public void Saut(){
-		
-		  if (tailleCheval== 1.60) {
+	public Cavalier(String nom) {
+		this.nom=nom;
+		this.tailleCheval=(Math.random()*(1.60 - 1.80));
+	}
+	
+	public void Saut(String TypeObstacle){
+		// taille minimun de cheval de 1.60  
+		switch(TypeObstacle) {
+		  case "1er type obstacle": // Le plus petit si on le passe pas eliminatoire
+			  if (tailleCheval== 1.60) {
+				  
+				    if ((Math.random()*100) > 30) // 70 pourcent de chance de sauter
+				    	{
+				    	score--;
+				    }
+				    else {
+				    	score-=2;
+				    	
+				    }
+				  }else if (tailleCheval==1.65) {
+					  if ((Math.random()*100) > 25) // 75 pourcent de chance de sauter
+					  {
+					    	score--;
+					    }
+					  else {
+						  score-=3;
+					  }
+				  }
+				  else {
+					  if ((Math.random()*100) > 20) {
+					    	score--;
+					    }else
+					    {
+					    	score-=4;
+					    }
+		    break;
+		  case "2eme type obtsacle": // Milieux de gammes 
+			  if (tailleCheval== 1.60) {
+				  
+				    if ((Math.random()*100) > 30) // 70 pourcent de chance de sauter
+				    	{
+				    	score--;
+				    }
+				    else {
+				    	score-=2;
+				    	
+				    }
+				  }else if (tailleCheval==1.65) {
+					  if ((Math.random()*100) > 25) // 75 pourcent de chance de sauter
+					  {
+					    	score--;
+					    }
+					  else {
+						  score-=3;
+					  }
+				  }
+				  else {
+					  if ((Math.random()*100) > 20) {
+					    	score--;
+					    }else
+					    {
+					    	score-=6;
+					    }
+		    break;
+		  default:// 3eme type obstacle les plus haut 
 			  
-		    if ((Math.random()*100) > 30) // 70 pourcent de chance de sauter
-		    	{
-		    	point--;
-		    }
-		    else {
-		    	point=0;
-		    	
-		    }
-		  }else if (tailleCheval==1.65) {
-			  if ((Math.random()*100) > 25) // 75 pourcent de chance de sauter
-			  {
-			    	point--;
-			    }
-			  else {
-				  point=0;
-			  }
-		  }
-		  else {
-			  if ((Math.random()*100) > 20) {
-			    	point--;
-			    }else
-			    {
-			    	point=0;
-			    }
-		  
+			  if (tailleCheval== 1.60) {
+				  
+				    if ((Math.random()*100) > 30) // 60 pourcent de chance de sauter
+				    	{
+				    	score--;
+				    }
+				    else {
+				    	score-=5;
+				    	
+				    }
+				  }else if (tailleCheval==1.65) {
+					  if ((Math.random()*100) > 25) // 75 pourcent de chance de sauter
+					  {
+					    	score--;
+					    }
+					  else {
+						  score-=10;;
+					  }
+				  }
+				  else {
+					  if ((Math.random()*100) > 20) {
+					    	score--;
+					    }else
+					    {
+					    	score=0;
+					    }
 		}
-		
-		
-		
-	}
-	
-	
-	
-	public String getNom(){
-		// methode retournant le nom
-		return nom ;
-	}
-	
-	
-
-}
